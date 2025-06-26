ab0c18c955c14e41ac62cf983a3d099020251b3a
KhaetbekIT <hayotbek-web@mail.ru>
2025-06-26 22:18:25 +0500

    fix: удалено не нужные дынне и поправлено .husky

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,8 +1,5 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import data from "@/data/admin.data.json";
 import { AppSidebar } from "./_components/app-sidebar";
-import { ChartAreaInteractive } from "./_components/chart-area-interactive";
-import { DataTable } from "./_components/data-table";
 import { SectionCards } from "./_components/section-cards";
 import { SiteHeader } from "./_components/site-header";
 
@@ -23,10 +20,6 @@ export default function Page() {
 					<div className="@container/main flex flex-1 flex-col gap-2">
 						<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 							<SectionCards />
-							<div className="px-4 lg:px-6">
-								<ChartAreaInteractive />
-							</div>
-							<DataTable data={data} />
 						</div>
 					</div>
 				</div>
